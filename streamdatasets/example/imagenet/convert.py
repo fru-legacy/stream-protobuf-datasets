799b3551bf3891ba7e3eb92496055523130a591b
fru <florian@rueberg.eu>
2021-12-15 16:17:26 +0000

    Do

--- streamdatasets/example/imagenet/convert.py
+++ streamdatasets/example/imagenet/convert.py
@@ -1,7 +1,8 @@
 from os import listdir
 from os.path import isfile, isdir, join
 
-folder = "/data/ILSVRC2017_CLS-LOC"
+#data/ILSVRC/Annotations/CLS-LOC/train/n02606052/n02606052_188.xml
+folder = "/data/ILSVRC/Annotations/CLS-LOC/train"
 onlyfolder = [f for f in listdir(folder) if isdir(join(folder, f))]
 
 print('Test123', len(onlyfolder))
