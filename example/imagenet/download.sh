8430ff8fd699b1d751e92562e9628bed15c03dd4
Florian Rüberg <florian.rueberg@gmail.com>
2021-12-15 12:35:40 +0200

    Create download.sh

---
+++ example/imagenet/download.sh
@@ -0,0 +1,13 @@
+sudo apt install transmission-cli
+
+# Visit https://academictorrents.com/browse.php?search=imagenet&sort_field=seeders&sort_dir=DESC
+# Get ILSVRC2017_CLS-LOC.tar.gz (Size 166.02GB) torrent url
+transmission-cli https://academictorrents.com/download/943977d8c96892d24237638335e481f3ccd54cfb.torrent
+
+# Get all processes with: ps -aux
+# Reconnect to process $pid with: sudo strace -ewrite -p $pid
+
+tar -xvzf ./Downloads/ILSVRC2017_CLS-LOC.tar.gz
+# Get progress with: ls ./Downloads/ILSVRC/Data/CLS-LOC/train/ | wc -l
+
+
