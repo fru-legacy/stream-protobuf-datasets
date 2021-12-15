570dd8352e8dcca3e0add852f7837dc32c91b6d0
fru <florian@rueberg.eu>
2021-12-15 17:19:49 +0000

    Do

--- streamdatasets/example/imagenet/convert.py
+++ streamdatasets/example/imagenet/convert.py
@@ -6,12 +6,16 @@ from bs4 import BeautifulSoup
 folder = "/data/ILSVRC/Annotations/CLS-LOC/train"
 train_folders = [join(folder, f) for f in listdir(folder) if isdir(join(folder, f))]
 
+values = {'difficult': []}
+
 for train_folder in train_folders[0:1]:
   files = [f for f in listdir(train_folder) if isfile(join(train_folder, f))]
   for file in files:
     with open(join(train_folder, file), 'r') as f:
       data = f.read()
     BsData = BeautifulSoup(data, "xml")
-    b_folder = ''.join(child for child in BsData.find_all('folder')[0].children)
+    b_folder = ''.join(child for child in BsData.find_all('difficult')[0].children)
+    if b_folder not in values['difficult']:
+      values['difficult'].append(b_folder)
     print(b_folder)
 
