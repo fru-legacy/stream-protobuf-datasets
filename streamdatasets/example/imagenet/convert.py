c8720230424c6b25d527af4f9d996f069908427b
fru <florian@rueberg.eu>
2021-12-16 11:31:27 +0000

    Do

--- streamdatasets/example/imagenet/convert.py
+++ streamdatasets/example/imagenet/convert.py
@@ -29,6 +29,8 @@ test_folder_data = join(in_dir, '/data/ILSVRC/Data/CLS-LOC/test')
 
 item_label_file = join(in_dir_kaggle, '/LOC_synset_mapping.txt')
 
+print(item_label_file)
+
 def _read_item_label_file(file):
   with open(file, 'r') as f:
     lines = [l.split(',') for l in file.readlines()]
