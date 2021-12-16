176dc969a6901ffa00ff8ee1340797e23fe8bf6e
fru <florian@rueberg.eu>
2021-12-16 17:50:24 +0000

    Do

--- streamdatasets/test_stream.py
+++ streamdatasets/test_stream.py
@@ -2,4 +2,8 @@ from .container.list import StreamDatasetList
 
 with open('/data/streamable/list.proto.bin', 'rb') as f:
   data = f.read()
-  StreamDatasetList().parse(data)
+
+list = StreamDatasetList().parse(data)
+print(len(list.lookup))
+print(len(list.items))
+print(len(list.items[10].buckets))
